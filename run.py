bdf0415a68ae95d12240842975252d5467cecb99
Dapunta ID <76211798+Dapunta@users.noreply.github.com>
2021-08-17 21:32:27 +0700

    Update run.py

--- run.py
+++ run.py
@@ -8,7 +8,7 @@ if sys.version[0:3] != "3.9":
 if __name__ == "__main__":
   try:
       os.system("git pull")
-      __import__("brute").folder()
+      __import__("elite").folder()
   except Exception:
     os.system("clear")
     print("\n Hello! Pengguna Script Elite")
